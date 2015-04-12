ff460932af6c73cf3fc28945ac9365b3a9814099
edward <edward.edward.dowling@gmail.com>
2015-04-12 22:19:23 +0100

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	core/assets/dick.png
    	core/assets/effects/Flame.p
    	core/assets/effects/particle.png
    	core/assets/effects/rainboom.p
    	core/assets/font/black16.fnt
    	core/assets/font/black16_0.png
    	core/assets/font/black32.fnt
    	core/assets/font/black32_0.png
    	core/assets/font/black64.fnt
    	core/assets/font/black64_0.png
    	core/assets/font/white16.fnt
    	core/assets/font/white16_0.png
    	core/assets/font/white32.fnt
    	core/assets/font/white32_0.png
    	core/assets/font/white64.fnt
    	core/assets/font/white64_0.png
    	core/assets/img/Thumbs.db
    	core/assets/img/badlogic.jpg
    	core/assets/img/player.jpg
    	core/assets/img/player.png
    	core/assets/jew.jpg
    	core/assets/maps/testMap.tmx
    	core/assets/maps/tile.png
    	core/assets/music/Jimmy-theme.mp3
    	core/assets/music/dankMusic.mp3
    	core/assets/music/introTheme.mp3
    	core/assets/testMap.tmx
    	core/assets/textures/Space2.jpg
    	core/assets/textures/Texture.png
    	core/assets/textures/badlogic.jpg
    	core/assets/textures/button.png
    	core/assets/textures/dick.png
    	core/assets/textures/introScreen.jpg
    	core/assets/textures/jew.jpg
    	core/assets/textures/player.png
    	core/assets/textures/tile.png
    	core/assets/tiles/block_pack.pack
    	core/assets/tiles/block_pack.png
    	core/assets/ui/atlas.pack
    	core/assets/ui/atlas.png
    	core/assets/ui/button-pack.pack
    	core/assets/ui/button-pack.png
    	core/assets/ui/button.pack
    	core/assets/ui/button.png
    	core/assets/ui/menuSkin.json

--- core/src/com/mygdx/game/MyContactListener.java
+++ core/src/com/mygdx/game/MyContactListener.java
@@ -9,32 +9,37 @@ import com.mygdx.game.things.projectiles;
 
 public class MyContactListener implements ContactListener {
 
- public void beginContact(Contact c) {
+    public void beginContact(Contact c) {
 
- Fixture fa = c.getFixtureA();
- Fixture fb = c.getFixtureB();
+        Fixture fa = c.getFixtureA();
+        Fixture fb = c.getFixtureB();
 
- if(fa.getBody().getUserData() != null && fa.getBody().getUserData() instanceof Flame && fb.getBody().getUserData() != TestClass.playerSprite  && fa.isSensor() == false) {
-     if(fb.getBody().getUserData() instanceof Flame){}else{
-         TestClass.getToDestroy().add(fa.getBody());
-     }
+        if (fa.getBody().getUserData() != null && fa.getBody().getUserData() instanceof Flame && fb.getBody().getUserData() != TestClass.playerSprite && fa.isSensor() == false) {
+            if (fb.getBody().getUserData() instanceof Flame) {
+            } else {
+                TestClass.getToDestroy().add(fa.getBody());
+            }
 
- }
- if(fb.getBody().getUserData() != null && fb.getBody().getUserData() instanceof Flame && fa.getBody().getUserData() != TestClass.playerSprite && fb.isSensor() == false){
-     if(fa.getBody().getUserData() instanceof Flame){}else{
-         TestClass.getToDestroy().add(fb.getBody());
-     }
- }
+        }
+        if (fb.getBody().getUserData() != null && fb.getBody().getUserData() instanceof Flame && fa.getBody().getUserData() != TestClass.playerSprite && fb.isSensor() == false) {
+            if (fa.getBody().getUserData() instanceof Flame) {
+            } else {
+                TestClass.getToDestroy().add(fb.getBody());
+            }
+        }
 
- }
+    }
 
- public void endContact(Contact c) {
- Fixture fa = c.getFixtureA();
- Fixture fb = c.getFixtureB();
+    public void endContact(Contact c) {
+        Fixture fa = c.getFixtureA();
+        Fixture fb = c.getFixtureB();
 
- }
+    }
 
- public void preSolve(Contact c, Manifold m) {}
- public void postSolve(Contact c, ContactImpulse ci) {}
+    public void preSolve(Contact c, Manifold m) {
+    }
 
- }
+    public void postSolve(Contact c, ContactImpulse ci) {
+    }
+
+}
