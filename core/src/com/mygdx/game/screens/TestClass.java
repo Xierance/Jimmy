a4eb46939823eeade5f08d67e70b07238eb5501f
Xierance <CianL2207@hotmail.cpm>
2015-05-03 20:19:50 +0100

    An archive of autism.

--- core/src/com/mygdx/game/screens/TestClass.java
+++ core/src/com/mygdx/game/screens/TestClass.java
@@ -2,6 +2,7 @@ package com.mygdx.game.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
@@ -17,6 +18,7 @@ import com.badlogic.gdx.physics.box2d.*;
 import com.badlogic.gdx.physics.box2d.joints.MouseJoint;
 import com.badlogic.gdx.physics.box2d.joints.MouseJointDef;
 import com.badlogic.gdx.utils.Array;
+import com.mygdx.game.Input;
 import com.mygdx.game.MyContactListener;
 import com.mygdx.game.MyGdxGame;
 import com.mygdx.game.Visuals.gameParticles.Explosion;
@@ -189,7 +191,12 @@ public class TestClass extends InputAdapter implements Screen {
 
         //input
 
-        Gdx.input.setInputProcessor((this));
+
+        Input input = new Input();
+        InputMultiplexer inputMultiplexer = new InputMultiplexer();
+        inputMultiplexer.addProcessor(this);
+        inputMultiplexer.addProcessor(input);
+        Gdx.input.setInputProcessor(inputMultiplexer);
 
         //ground
         BodyDef tempdef = new BodyDef();
@@ -356,20 +363,20 @@ public class TestClass extends InputAdapter implements Screen {
                         new Vector2(getmouseCoords().x, getmouseCoords().y)).getBody());
 
         //move
-        if (this.Space && player.isPlayerGrounded(world, player)) {
+        if (Input.Space && player.isPlayerGrounded(world, player)) {
             player.getPlayerBody().applyLinearImpulse(new Vector2(0, 3), new Vector2(), true);
         }
 
-        if (this.S) {
+        if (Input.S) {
             player.getPlayerBody().applyForceToCenter(new Vector2(0, -20), true);
         }
 
-        if (this.A) {
+        if (Input.A) {
             player.getPlayerBody().applyForceToCenter(new Vector2(-20, 0), true);
 
         }
 
-        if (this.D) {
+        if (Input.D) {
             player.getPlayerBody().applyForceToCenter(new Vector2(20, 0), true);
         }
 
@@ -454,150 +461,6 @@ public class TestClass extends InputAdapter implements Screen {
         return true;
     }
 
-    public boolean keyDown(int keycode) {
-        switch (keycode) {
-            case 19:
-                Up = true;
-                break;
-            case 20:
-                Down = true;
-                break;
-            case 21:
-                Left = true;
-                break;
-            case 22:
-                Right = true;
-                break;
-            case 62:
-                Space = true;
-                break;
-            case 41:
-                M = true;
-                break;
-            case 43:
-                O = true;
-                break;
-            case 46:
-                R = true;
-                break;
-            case 33:
-                E = true;
-                break;
-            case 47:
-                S = true;
-                break;
-            case 29:
-                A = true;
-                break;
-            case 42:
-                N = true;
-                break;
-            case 37:
-                I = true;
-                break;
-            case 31:
-                C = true;
-                break;
-            case 44:
-                P = true;
-                break;
-            case 40:
-                L = true;
-                break;
-            case 131:
-                Escape = true;
-                break;
-            case 51:
-                W = true;
-                break;
-            case 32:
-                D = true;
-                break;
-            case 129:
-                Ctrl_left = true;
-                break;
-            case 130:
-                Ctrl_right = true;
-                break;
-            case 45:
-                Q = true;
-                break;
-        }
-        return true;
-    }
-
-    public boolean keyUp(int keycode) {
-        switch (keycode) {
-            case 19:
-                Up = false;
-                break;
-            case 20:
-                Down = false;
-                break;
-            case 21:
-                Left = false;
-                break;
-            case 22:
-                Right = false;
-                break;
-            case 62:
-                Space = false;
-                break;
-            case 41:
-                M = false;
-                break;
-            case 43:
-                O = false;
-                break;
-            case 46:
-                R = false;
-                break;
-            case 33:
-                E = false;
-                break;
-            case 47:
-                S = false;
-                break;
-            case 29:
-                A = false;
-                break;
-            case 42:
-                N = false;
-                break;
-            case 37:
-                I = false;
-                break;
-            case 31:
-                C = false;
-                break;
-            case 44:
-                P = false;
-                break;
-            case 40:
-                L = false;
-                break;
-            case 131:
-                Escape = false;
-                break;
-            case 51:
-                W = false;
-                break;
-            case 32:
-                D = false;
-            case 129:
-                Ctrl_left = false;
-                break;
-            case 130:
-                Ctrl_right = false;
-                break;
-            case 45:
-                Q = false;
-                break;
-
-        }
-
-        return true;
-    }
 
     public Vector2 getmouseCoords() {
         Vector3 temp = new Vector3();
